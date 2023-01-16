53048c6b97b11bc4c9857a290af0bc3d47d36b05
Alex Bojorquez <Alex.bojorquez2598@gmail.com>
2023-01-15 20:12:30 -0500

    restyled the mobile nav so it fits with the challenge's design

--- dist/js/index.js
+++ dist/js/index.js
@@ -1,7 +1,8 @@
 const featuresBtn = document.getElementById("features-btn");
 const companyBtn = document.getElementById("company-btn");
-const hamburger = document.querySelector(".nav__hamburger");
-const navList = document.querySelector(".nav__list");
+const hamburger = document.querySelector(".hamburger");
+const nav = document.querySelector(".nav");
+const navClose = document.querySelector(".nav__close");
 
 function toggleDropdown() {
   this.classList.toggle("nav__btn--open");
@@ -9,11 +10,12 @@ function toggleDropdown() {
   dropdown.classList.toggle("dropdown--open");
 }
 
-featuresBtn.addEventListener("click", toggleDropdown);
+function toggleNav() {
+  nav.classList.toggle("open");
+}
 
+featuresBtn.addEventListener("click", toggleDropdown);
 companyBtn.addEventListener("click", toggleDropdown);
 
-hamburger.addEventListener("click", function () {
-  this.classList.toggle("nav__hamburger--open");
-  navList.classList.toggle("open");
-});
+hamburger.addEventListener("click", toggleNav);
+navClose.addEventListener("click", toggleNav);
